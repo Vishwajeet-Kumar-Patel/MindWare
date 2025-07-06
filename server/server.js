d76d1dae2608174574852995e328aace9c1a0514
Vishwajeet Kumar <vishwajeetkumarpatelmgs@gmail.com>
2025-07-06 15:18:49 +0530

    CORS fixed

--- server/server.js
+++ server/server.js
@@ -12,11 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://mind-ware.vercel.app', // ✅ allow your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/quiz', quizRoutes);
